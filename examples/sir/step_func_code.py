2a2c123b6b05ad733f21b71f3073bc607b596bfc
co1 <gunaratnecs@ornl.gov>
2025-02-27 16:29:34 -0500

    merge conflict fixed for sir example batch script

--- examples/sir/step_func_code.py
+++ examples/sir/step_func_code.py
@@ -9,8 +9,6 @@ from cupyx import jit
 
 @jit.rawkernel(device='cuda')
 def step_func(id, id2index, globals, breeds, locations, states):
-    def my_func(a):
-        a[0] = 3
     #agent_index = step_func_helper_get_agent_index(id, id2index)
     # nan checked by inequality to self. Unfortunate limitation of cupyx
     if (id == id) and (id2index[int(id)] == id2index[int(id)]):
@@ -29,9 +27,6 @@ def step_func(id, id2index, globals, breeds, locations, states):
                     neighbor_state = states[neighbor_index]
                     if neighbor_state == 2 and rand < p_infection:
                         states[agent_index] = 2.0
-
-        
-        my_func(states)
 
     
 
