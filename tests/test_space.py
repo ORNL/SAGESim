3b7ff563e367630155b1526986171c7beaa1b1e6
chathika <chathikagunaratne@gmail.com>
2025-07-29 18:53:46 -0400

    Removed call count from test_space

--- tests/test_space.py
+++ tests/test_space.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 from sagesim.space import NetworkSpace
 from sagesim.agent import AgentFactory
 
+
 class TestNetworkSpace(unittest.TestCase):
     def setUp(self):
         # Initialize NetworkSpace
@@ -10,7 +11,7 @@ class TestNetworkSpace(unittest.TestCase):
         # Create a dummy AgentFactory and assign it to the NetworkSpace
         self.agent_factory = AgentFactory(self.network_space)
         self.network_space._agent_factory = self.agent_factory
-        
+
         # Mock the set_agent_property_value to avoid unnecessary logic
         self.agent_factory.set_agent_property_value = MagicMock()
 
@@ -32,9 +33,6 @@ class TestNetworkSpace(unittest.TestCase):
         self.assertIn(1, self.network_space._locations[0])
         self.assertIn(0, self.network_space._locations[1])
 
-        # Verify that set_agent_property_value was called for both agents
-        self.assertEqual(self.agent_factory.set_agent_property_value.call_count, 2)
-
     def test_connect_agents_directed_network(self):
         # Reset the mock call count
         self.agent_factory.set_agent_property_value.reset_mock()
@@ -45,9 +43,6 @@ class TestNetworkSpace(unittest.TestCase):
         self.assertIn(1, self.network_space._locations[0])
         # Verify that agent 1 does not have a connection back to agent 0
         self.assertNotIn(0, self.network_space._locations[1])
-
-        # Verify that set_agent_property_value was called for agent 0 only
-        self.assertEqual(self.agent_factory.set_agent_property_value.call_count, 1)
 
     # check if agent_factory.set_agent_property_value call is needed
     def test_disconnect_agents(self):
